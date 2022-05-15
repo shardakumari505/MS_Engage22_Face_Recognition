d6362da24e432a0fbefe4e15ef97ba10fbe5f0b8
Sharda Kumari <shardakumari505@gmail.com>
2022-05-15 23:05:42 +0530

    Created background and designed the theme for homepage of the website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from './pages/dashboard-page/dashboard-page';
+import Navbar from './components/navbar-component/navbar-component';
 
-function App() {
+class App extends Component {
+
+
+
+  render(){
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+          <Router>
+            <Navbar/>
+              <Routes>
+                  <Route path="/" element={<Dashboard />} />
+              </Routes>
+          </Router>   
+      
+      </div>
+    );
+}
 }
 
 export default App;
---
+++ src/components/navbar-component/navbar-component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './navbar-component.styles.scss';
+
+const Navbar = () => {
+    return(<div className='navbar-container'> 
+        
+     </div> )
+}
+
+export default Navbar;
