8bf19ac38075f960866b0757fb9925e976c5d768
Sharda Kumari <shardakumari505@gmail.com>
2022-05-16 14:07:02 +0530

    Added navbar and created dark and light theme for the website

--- src/App.js
+++ src/App.js
@@ -1,27 +1,65 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from './pages/dashboard-page/dashboard-page';
 import Navbar from './components/navbar-component/navbar-component';
+import {UserProvider} from './userauth';
+import styled, {ThemeProvider} from "styled-components";
+import {lightTheme, darkTheme, GlobalStyles} from "./themes.js";
+import { CgSun } from "react-icons/cg";
+import { HiMoon } from "react-icons/hi";
 
-class App extends Component {
+const StyledApp = styled.div`
+  color: ${(props) => props.theme.fontColor};
 
+`;
 
+const Toggle = styled.button`
+    cursor: pointer;
+    z-index: 15;
+    position: absolute;
+    bottom: 5vh;
+    right: 5vw;
+    height: 50px;
+    width: 50px;   
+    border-radius: 50%;
+    border: none;
+    background-color: ${props => props.theme.titleColor};
+    color: ${props => props.theme.pageBackground};
+    &:focus {
+        outline: none;
+    }
+    transition: all .5s ease;
+`;
 
-  render(){
-   
+function App() {
+
+  const [theme, setTheme] = useState("light");
+    const themeToggler = () =>{
+        theme === "light" ? setTheme("dark") : setTheme("light");
+    };
+
+  const icon = theme === "light" ? <HiMoon size={40} /> : <CgSun size={40} />;
+
   return (
-      <div className="App">
-          <Router>
-            <Navbar/>
-              <Routes>
-                  <Route path="/" element={<Dashboard />} />
-              </Routes>
-          </Router>   
-      
-      </div>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <StyledApp>
+        <div className="App">
+          <UserProvider>
+            <Router>
+              <Navbar/>
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                </Routes>
+            </Router>   
+          </UserProvider>
+          <Toggle onClick={() => themeToggler()}>{icon}</Toggle>
+        </div>
+      </StyledApp>
+    </ThemeProvider>
     );
-}
+
 }
 
 export default App;
--- src/components/navbar-component/navbar-component.js
+++ src/components/navbar-component/navbar-component.js
@@ -1,10 +1,69 @@
-import React from 'react';
+import React,{useContext, useState} from 'react';
 import './navbar-component.styles.scss';
+import { NavLink } from 'react-router-dom';
+import { Link } from "react-scroll";
+import { UserContext } from '../../userauth';
 
 const Navbar = () => {
-    return(<div className='navbar-container'> 
-        
-     </div> )
-}
+    const [loggedIn,setloggedIn] = useContext(UserContext);
+    const [MenuList, setMenulist] = useState(false);
+    
 
+    const handleLogout = (event) =>{
+        event.preventDefault();
+        if(loggedIn===true){
+        setloggedIn(false)
+        alert("Logged Out Sucessfully");}
+        else{
+            alert("Please SignIn to Logout")
+        }
+
+    }
+    
+
+    // const handleclick1 = (event) =>{
+    //     event.preventDefault();
+    //     setloggedIn(true)
+    //     alert("Logged In Sucessfully");
+    // }
+    const toggleMenu = (event) => {
+        setMenulist(!MenuList);
+    }
+
+    
+    return(
+            <div className='navbar-container'>
+        
+                <h1><NavLink className='navbar-logo' to="/"> &lt; DevIN &gt; </NavLink></h1>
+                    <nav>                        
+                        <ul id='menu-list' className= {MenuList===true?'menu-list':'menu-list-closed'}>
+                            <li>
+                                <NavLink className='' to="/">Home</NavLink>
+                            </li>
+                            <li>
+                                <NavLink className='' to="/">Tech Blogs</NavLink>
+                            </li>
+                            <li>
+                                <NavLink className='' to="/">Virtual Chamber</NavLink>
+                            </li>
+                            
+                            <li>
+                                <NavLink className='' to="/">Code Editor</NavLink>
+                            </li>
+                    
+                    <li> {loggedIn===false?
+                    <button><NavLink className='navbar-login-button-text' to="/signin">
+                    Login | Signup                                
+                                    </NavLink></button>:<button onClick={handleLogout} className='navbar-login-button-text'>Signout</button>}</li>
+                                               
+                                
+
+                             </ul>
+                    </nav>
+                <h1 className='hamburger-menu-icon' onClick={toggleMenu}>☰</h1>
+            </div>
+           
+    );
+
+}
 export default Navbar;
